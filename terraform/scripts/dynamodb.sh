8f2898b3bb37ba942524df66fb9256fe0bde584d
lucasdavip <lucasdavip@hotmail.com>
2021-02-06 22:24:46 -0300

    Ajuste scripts

--- terraform/scripts/dynamodb.sh
+++ terraform/scripts/dynamodb.sh
@@ -1,7 +1,8 @@
 # Criar tabela state lock (dynamodb)
-env=$1
+resource=$1
+env=$2
 aws dynamodb create-table \
-    --table-name terraform_${env} \
+    --table-name ${resource}_terraform_${env} \
     --attribute-definitions \
         AttributeName=LockID,AttributeType=S \
     --key-schema AttributeName=LockID,KeyType=HASH \
---
+++ terraform/scripts/install_terraform.sh
@@ -0,0 +1,1 @@
+docker run -it -v /home/lgomes/github/eks-cluster/terraform/scripts:/app -w /app --entrypoint   hashicorp/terraform:light sh
