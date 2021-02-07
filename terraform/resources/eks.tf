94c92750bb466e0f522d4c1eff62db465de0ff13
lucasdavip <lucasdavip@hotmail.com>
2021-02-06 21:16:57 -0300

    Ajuste vpc

---
+++ terraform/resources/eks.tf
@@ -0,0 +1,12 @@
+/*
+Neded resources
+
+1. Image aws
+2. Launch configuration
+3. Auto scaling group
+4. EKS Cluster
+5. eksctl
+6. kubectl
+
+
+*/
--- terraform/resources/output.tf
+++ terraform/resources/output.tf
@@ -14,6 +14,10 @@ output "subnet_id_pub1b" {
   value = module.create_subnet_pub1b.subnet_id
 }
 
+output "subnet_id_pub1c" {
+  value = module.create_subnet_pub1c.subnet_id
+}
+
 output "subnet_id_priv1a" {
   value = module.create_subnet_priv1a.subnet_id
 }
@@ -22,12 +26,20 @@ output "subnet_id_priv1b" {
   value = module.create_subnet_priv1b.subnet_id
 }
 
+output "subnet_id_priv1c" {
+  value = module.create_subnet_priv1c.subnet_id
+}
+
 output "subnet_id_priv2a" {
   value = module.create_subnet_priv2a.subnet_id
 }
 
 output "subnet_id_priv2b" {
   value = module.create_subnet_priv2b.subnet_id
+}
+
+output "subnet_id_priv2c" {
+  value = module.create_subnet_priv2c.subnet_id
 }
 
 output "Internet_gateway" {
@@ -40,4 +52,8 @@ output "natgw_pub1a_id" {
 
 output "natgw_pub1b_id" {
   value = module.create_natgw_pub1b.natgw_id
+}
+
+output "natgw_pub1c_id" {
+  value = module.create_natgw_pub1c.natgw_id
 }
